f375658ca8838ee81fb91880aef7637c69b264c4
rintisch <gerald.rintisch@posteo.de>
2025-04-27 10:30:47 +0200

    Remove superflouous files

--- rector.php
+++ rector.php
@@ -22,7 +22,6 @@ return RectorConfig::configure()
         __DIR__ . '/Tests',
         __DIR__ . '/ext_emconf.php',
         __DIR__ . '/ext_localconf.php',
-        __DIR__ . '/ext_tables.php',
     ])
     ->withPhpVersion(PhpVersion::PHP_82)
     ->withPhpSets(
